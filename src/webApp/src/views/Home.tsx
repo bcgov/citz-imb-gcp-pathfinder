5ac34e45bb6e082aa154e5ba4e2973abb0712f24
Jayna Bettesworth <bettesworthjayna@gmail.com>
2023-07-04 11:53:59 -0700

    Docker Form and Navigation

--- src/webApp/src/views/Home.tsx
+++ src/webApp/src/views/Home.tsx
@@ -1,21 +1,101 @@
-import { useState } from "react"
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+
 function Home() {
-    const [count, setCount] = useState(0)
+  const [dockerFile, setDockerFile] = useState("");
+  const [accessCode, setAccessCode] = useState("");
+  const [name, setName] = useState("");
+  const [splitDockerName, setSplitDockerName] = useState("");
+  const navigate = useNavigate();
+
+  /**
+  * @description Used to save dockerfile and remove path recieved in input.
+  * @param {React.ChangeEvent}     e event from HTML input element
+  */
+  async function saveDockerFile(e: React.ChangeEvent<HTMLInputElement>){
+    setDockerFile(e.target.value)
+    const splitArray = (e.target.value).split("\\");
+    setSplitDockerName(splitArray[splitArray.length - 1])
+  }
+  
+
+  /**
+  * @description Submit form, and redirect.
+  * @param {React.FormEvent}     event event from HTML Form element
+  */
+  async function submitForm(event: React.FormEvent<HTMLFormElement>){
+    event.preventDefault();
+    navigate('/info')
+  }
 
   return (
     <>
       <h1>Docker Deploy</h1>
+      <p className="read-the-docs">
+        Insert your docker file to deploy it to google cloud
+      </p>
+      
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <br/>
+        <form onSubmit={((e) =>submitForm(e))}>
+        
+        <label >  Enter Name: 
+          </label>
+          <br/> 
+          <input 
+            type="text" 
+            value={name} 
+            style={{ }} 
+            onChange={((e) => setName(e.target.value))}
+          />
+          <br/>
+          <br/>
+          <br/>
+          
+          <label style={{
+            backgroundColor: "#e0e0e0", 
+            padding: "10px 17px", 
+            borderRadius: "5px"
+          }}>
+            <input 
+              type="file"
+              value={dockerFile}
+              style={{display: "none"}}
+              onChange={((e) => saveDockerFile(e))}
+            />
+            {splitDockerName ? ("Change "):("Add ")}
+             Docker File
+          </label>
+          <br/>
+          <br/>
+          {splitDockerName}
+          
+          <br/>
+          <br/>
+          <label >  Enter Access Code: 
+          </label>
+          <br/> 
+          <input 
+            type="text" 
+            value={accessCode} 
+            style={{ }} 
+            onChange={((e) => setAccessCode(e.target.value))}
+          />
+          <br/>
+          
 
+          <br/>
+          <br/>
+          <button type='submit'> 
+            submit
+          </button>
+        </form>
+        <br/>
+        <p className="read-the-docs">
+        Once you submit, wait 1 - 3 minutes for a http link to be returned to you
+      </p>
         
       </div>
-      <p className="read-the-docs">
-        Insert your docker file to deploy it
-      </p>
+      
       </>
   )
 
